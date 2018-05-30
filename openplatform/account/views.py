961d4da923c9f70362028642b9e2693c094c21d8
freez91 <useyes91@gmail.com>
2018-05-30 14:48:16 +0800

    delete bot_token

--- openplatform/account/views.py
+++ openplatform/account/views.py
@@ -21,11 +21,6 @@ from bixin.helpers import ok_json, error_json
 def home(request):
     if request.bx_user.is_authenticated():
         return redirect(reverse('account:index'))
-
-    bot_token = request.GET.get('bot_token')
-    if bot_token:
-        return bot_login(request, bot_token)
-
     return render(request, 'home.html', locals())
 
 @login_required
@@ -38,25 +33,6 @@ def index(request):
         fund = funds.first()
 
     return render(request, 'index.html', locals())
-
-def bot_login(request, bot_token):
-
-    bot = Bot()
-    expire = 60 * 60 * 24
-    platform_user_id = bot.valid_token(bot_token, expire=expire)
-    if not platform_user_id:
-        logging.warn("Invalid  bot_token {} return 401".format(bot_token))
-        return error_json('Invalid bot token', status=401)
-
-    c = Client()
-    user = c.get_user(platform_user_id, cache=True)
-
-    request.session['expire'] = int(time.time()) + expire
-    request.session['site_userid'] = user.id
-    request.session.save()
-    site_userid = user.id
-
-    return redirect(reverse('account:index'))
 
 @tag_device
 def qr_session(request):
